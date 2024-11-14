32eca35f12a810fd27d0b69b4b67156d63375fc6
emkodelirdi <emirpartal@gmail.com>
2024-11-14 17:53:34 +0300

    Added not moving worker

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.Input;
 
 public class Mover : MonoBehaviour
 {
+    float moveSpeed = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,13 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input
+        //Get the value of the Horizontal input axis.
+
+        float verticalInput = Input.GetAxis("Vertical");
+        //Get the value of the Vertical input axis.
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+        //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
     }
 }
