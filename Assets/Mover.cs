6e2f89fbcbc16e08fc2654490599d51609788481
emkodelirdi <emirpartal@gmail.com>
2024-11-23 16:04:44 +0300

    Removed old code
    Removed not working code

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,25 +1,20 @@
 using UnityEngine;
-using UnityEngine.Input;
+using UnityEngine.InputSystem;
 
 public class Mover : MonoBehaviour
 {
     float moveSpeed = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private PlayerInputActions inputActions;
+
+
     void Start()
     {
-        
+        inputActions = new PlayerInputActions()
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        float horizontalInput = Input
-        //Get the value of the Horizontal input axis.
-
-        float verticalInput = Input.GetAxis("Vertical");
-        //Get the value of the Vertical input axis.
-
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
-        //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
+        
     }
 }
