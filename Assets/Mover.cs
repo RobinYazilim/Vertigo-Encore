68d9bd1332a5c7e75bda2690d65739b97c6b20c2
emkodelirdi <emirpartal@gmail.com>
2024-11-23 19:58:37 +0300

    Removed logs
    and old code

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -28,20 +28,5 @@ public class Mover : MonoBehaviour
         Vector2 moveVector = movement * moveSpeed * Time.deltaTime;
 
         rb.MovePosition(rb.position + moveVector);
-        //rb.linearVelocity = moveVector;
-    }
-
-    // Optional: Check for collisions with other colliders (if using triggers)
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        // Handle logic when colliding with another trigger collider
-        Debug.Log("Triggered with: " + other.gameObject.name);
-    }
-
-    // Optional: Check for collisions if not using triggers
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Handle logic when colliding with another collider
-        Debug.Log("Collided with: " + collision.gameObject.name);
     }
 }
