d9099f81bf852564ae493a968a7fc50924f77e65
emkodelirdi <emirpartal@gmail.com>
2024-11-23 19:56:20 +0300

    Added movement script

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -3,18 +3,45 @@ using UnityEngine.InputSystem;
 
 public class Mover : MonoBehaviour
 {
-    float moveSpeed = 10;
-    private PlayerInputActions inputActions;
-
+    [SerializeField]
+    private float moveSpeed = 10f;
+    private InputAction moveAction;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        inputActions = new PlayerInputActions()
+        var playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Player/Walk"];
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
         
+    }
+    void FixedUpdate()
+    {
+        Vector2 movement = moveAction.ReadValue<Vector2>();
+
+        Vector2 moveVector = movement * moveSpeed * Time.deltaTime;
+
+        rb.MovePosition(rb.position + moveVector);
+        //rb.linearVelocity = moveVector;
+    }
+
+    // Optional: Check for collisions with other colliders (if using triggers)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Handle logic when colliding with another trigger collider
+        Debug.Log("Triggered with: " + other.gameObject.name);
+    }
+
+    // Optional: Check for collisions if not using triggers
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Handle logic when colliding with another collider
+        Debug.Log("Collided with: " + collision.gameObject.name);
     }
 }
